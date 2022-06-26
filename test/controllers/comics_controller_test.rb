a90baaad30cc1ecedc2df062b7330dd09c7a62cf
bernar40 <bernardo.ruga@hotmail.com>
2022-06-26 18:05:21 -0300

    testing comics controller

--- test/controllers/comics_controller_test.rb
+++ test/controllers/comics_controller_test.rb
@@ -1,7 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class ComicsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get root_path
+    assert_response :success
+    assert_select 'title', 'Marvel - Rails'
+  end
+
+  test 'should get search' do
+    get comics_search_path, params: { hero_name: 'Test Hero 1' }
+    assert_response :success
+  end
 end
